7abb38176282ebb42792cd4adf4732fe988d79c4
a1767352 <a1767352@ac1c0196-ca05-47d2-82a5-85efdb603651>
2020-05-20 13:57:19 +0000

    added part2

--- 2020/s1/spc/assignment3/execute.c
+++ 2020/s1/spc/assignment3/execute.c
@@ -17,7 +17,7 @@ int execute(char *argv[])
 {
 	int length    = 0;
 	int isPipe    = 0;
-	//int isFIn     = 0;
+	int isFIn     = 0;
 	int isFOut    = 0;
 
 	int * pipePos = (int*)malloc(sizeof(int));
@@ -26,7 +26,11 @@ int execute(char *argv[])
 	int * FOutPos = (int*)malloc(sizeof(int));
 	int FOutSize  = 0;
 
+	int * FInPos = (int*)malloc(sizeof(int));
+	int FInSize  = 0;
+
 	int Fptr;
+	int Fptr2;
 
 	while (argv[length] != NULL)
 	{
@@ -47,11 +51,15 @@ int execute(char *argv[])
 
 			FOutSize++;
 		}
-		/*
+		
 		if (*argv[length] == '<')
 		{
 			isFIn = 1;
-		}*/
+			FInPos = (int*)realloc(FInPos,sizeof(int)*(FInSize + 1));
+			FInPos[FInSize] = length;
+
+			FInSize++;
+		}
 
 		length++;
 	}
@@ -77,7 +85,7 @@ int execute(char *argv[])
 			signal(SIGINT, SIG_DFL);
 			signal(SIGQUIT, SIG_DFL);
 			
-			if (isFOut == 1)
+			if (isFOut == 1 && isFIn == 0)
 			{       
 				argv[1] = NULL;
 				Fptr = open(argv[FOutPos[0]+1], O_WRONLY | O_CREAT/*, S_IRWXU*/);
@@ -85,10 +93,17 @@ int execute(char *argv[])
 				
 				execvp(argv[0], argv);
 			}
-			else
+			
+			if (isFIn == 1 && isFOut == 0)
 			{
+				argv[1] = NULL;
+				Fptr = open(argv[FInPos[0]+1], O_RDONLY | O_CREAT/*, S_IRWXU*/);
+				dup2(Fptr,STDIN_FILENO);
+
 				execvp(argv[0], argv);
 			}
+
+			execvp(argv[0], argv);
 
 			perror("cannot execute command");
 			exit(1);
@@ -99,12 +114,6 @@ int execute(char *argv[])
 			{
 				perror("wait");
 			}
-
-			if (isFOut == 1)
-			{
-				dup2(STDOUT_FILENO,Fptr);
-				close(Fptr);
-			}		
 		}
 		
 		return child_info;
@@ -123,7 +132,6 @@ int execute(char *argv[])
 	
 		for (int i = 0; i < pipeSize + 1; i++)
 		{
-			//printf("\n### next iteration\n\n");
 
 			int delete = 0;
 			int process = fork();
@@ -132,35 +140,38 @@ int execute(char *argv[])
 			{
 				if (i == 0)
 				{
-					//printf("First Piping\n");
 					commands = (char**)malloc( (pipePos[0] * sizeof(char*)) + sizeof(char*) );
-					//printf("%d - %d = %d\n",pipePos[0], 0, pipePos[0] - 0);
-
+					
 					for (int j = 0; j < pipePos[0]; j++)
 					{
 						commands[j] = argv[j];
-						//printf("%s\n",commands[j]);
+
 						delete++;
 					}
 					commands[pipePos[0]] = NULL;
 
-					//piping and then execvp
 					dup2(firstPipe[1],STDOUT_FILENO);
 					close(firstPipe[0]);
+
+					if (isFIn == 1)
+					{
+						commands[FInPos[0]] = NULL;
+						Fptr = open(commands[pipePos[0]-1], O_RDONLY | O_CREAT/*, S_IRWXU*/);
+						dup2(Fptr,STDIN_FILENO);
+					}
 					
 					execvp(commands[0],commands);
 				}
 				
 				if (i > 0 && i + 1 != pipeSize + 1)
 				{
-					//printf("Mid Piping\n");
+
 					commands = (char**)malloc( ( ((pipePos[i] - 1) - pipePos[i - 1]) * sizeof(char*) ) + sizeof(char*) );
-					//printf("%d - %d = %d\n",pipePos[i] - 1, pipePos[i - 1], (pipePos[i] - 1) - pipePos[i - 1]);
+
 
 					for (int j = 0; j < pipePos[i] - pipePos[i - 1] - 1; j++)
 					{
 						commands[j] = argv[pipePos[i - 1] + j + 1];
-						//printf("%s\n",commands[j]);
 						delete++;
 					}
 					commands[pipePos[i] - pipePos[i - 1]] = NULL;
@@ -189,19 +200,23 @@ int execute(char *argv[])
 				
 				if (i + 1 == pipeSize + 1)
 				{
-					//printf("Last Piping\n");
-
-					//printf("%d - %d = %d\n",(length) , pipePos[i - 1], (length) - pipePos[i - 1]);
 					commands = (char**)malloc( ( ((length) - pipePos[i - 1]) * sizeof(char*) ) + sizeof(char*) );
 
 					for (int j = 0; j < (length - 1) - pipePos[i - 1]; j++)
 					{
 						commands[j] = argv[pipePos[i - 1] + j + 1];
-						//printf("%s\n",commands[j]);
+
 						delete++;
 					}
 
 					commands[(length) - pipePos[i - 1]] = NULL;
+
+					if (isFOut == 1)
+					{       
+						commands[(length) - pipePos[i - 1] - 3] = NULL;
+						Fptr2 = open(commands[FOutPos[0]+1], O_WRONLY | O_CREAT/*, S_IRWXU*/);
+						dup2(Fptr2,STDOUT_FILENO);
+					}
 
 					if (alternate % 2 != 0)
 					{
@@ -228,6 +243,7 @@ int execute(char *argv[])
 					dup2(STDOUT_FILENO,firstPipe[1]);
 					alternate++;
 				}
+
 				if (i > 0 && i + 1 != pipeSize + 1)
 				{
 					if (alternate % 2 != 0)
@@ -244,6 +260,7 @@ int execute(char *argv[])
 					}
 					alternate++;
 				}
+
 				if (i + 1 == pipeSize + 1)
 				{
 					if (alternate % 2 != 0)
--- 2020/s1/spc/assignment3/makefile
+++ 2020/s1/spc/assignment3/makefile
@@ -1,4 +1,4 @@
 all: part1
 
 part1:
-	gcc -o smsh2 smsh2.c splitline.c execute.c -std=c99
+	gcc -o smsh2 smsh2.c splitline.c execute.c -Wall -std==c99
